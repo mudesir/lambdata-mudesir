aa22b1ac782bac4f3099188514b45912d12f0ac0
Mudesir Suleyman <nunasuleyman.gmail.com>
2021-02-08 22:09:05 -0800

    First module of spring 1 for DS23

---
+++ lambdata/__init__.py
@@ -0,0 +1,11 @@
+""" Lambda - A collection of Data Science Helper functions
+"""
+
+import pandas
+import numpy
+
+
+def df_cleanser(df):
+    """Clean pd.DataFrame"""
+    pass
+    
---
+++ lambdata/example_module.py
@@ -0,0 +1,3 @@
+"""A module Lambdata"""
+
+FAVORITE_ANIMALS = ["tree frog", "white rhino", "zibra"]
